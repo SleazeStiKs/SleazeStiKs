a827dcaa9c62ea22af4ca0e8ab536f4f06191402
Sleaze <sleazestiks@gmail.com>
2023-02-15 20:14:17 +0100

    Update update_birthday.py

--- update_birthday.py
+++ update_birthday.py
@@ -1,6 +1,7 @@
 import pathlib
 import re
-from datetime import date
+from datetime import datetime, date
+import dateutil
 
 root = pathlib.Path(__file__).parent.resolve()
 
@@ -15,14 +16,21 @@ def replace_writing(content, marker, chunk, inline=False):
     chunk = '<!-- {} starts -->{}<!-- {} ends -->'.format(marker, chunk, marker)
     return r.sub(chunk, content)
 
+def birthday():
+    # Get the current date
+    now = datetime.datetime.utcnow()
+    now = now.date()
+
+    # Get the difference between the current date and the birthday
+    age = dateutil.relativedelta.relativedelta(now, date(2005, 4, 3))
+    age = age.years
+
+    return age
 
 if __name__ == '__main__':
-    # Get Age
-    age = date.today().year - 2005;
-
     readme_path = root / 'README.md'
     readme = readme_path.open().read()
     
     # Update Age
-    rewritten_count = replace_writing(readme, 'age', age, inline=True)
+    rewritten_count = replace_writing(readme, 'age', birthday(), inline=True)
     readme_path.open('w').write(rewritten_count)
